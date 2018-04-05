f553cdd01f72fee0a5c959835dd63d6118e45efb
Arun M <arun11299@gmail.com>
2018-04-05 16:38:14 +0530

    Finally got linked list iteration working

--- data_structures/list/src/bin/main.rs
+++ data_structures/list/src/bin/main.rs
@@ -1,9 +1,25 @@
 extern crate list;
 use list::List;
 
+fn print(l : &List<i32>)
+{
+    let mut n = &l.head;
+    loop {
+        let tmp = n;
+        if let Some(ref node) = *tmp {
+            print!("{:?} ", node.value);
+            n = &node.next;
+        } else {
+            break;
+        }
+    }
+}
+
 fn main() {
     let mut l = List::new();
     l.append(1);
     l.append(2);
     l.append(3);
+
+    print(&l);
 }
--- data_structures/list/src/lib.rs
+++ data_structures/list/src/lib.rs
@@ -1,7 +1,11 @@
+use std::boxed::Box;
+use std::mem;
+use std::cell::RefCell;
+
 pub struct Node<T>
 {
-    value : T,
-    next  : Option<Box<Node<T>>>,
+    pub value : T,
+    pub next  : Option<Box<Node<T>>>,
 }
 
 impl<T> Node<T> {
@@ -14,7 +18,22 @@ impl<T> Node<T> {
         }
     }
     ///
-    fn append(&mut self, value : T)
+    fn append(&mut self, value: T)
+    {
+        let mut anchor = &mut self.next;
+        loop {
+            let tmp = anchor;
+            if let Some(ref mut n) = *tmp {
+                anchor = &mut n.next;
+            } else {
+                anchor = tmp;
+                break;
+            }
+        }
+        *anchor = Some(Box::new(Node::new(value)));
+    }
+    ///
+    fn append_rec(&mut self, value : T)
     {
         loop {
             if self.next.is_none() {
@@ -33,8 +52,8 @@ impl<T> Node<T> {
 
 pub struct List<T>
 {
-    len  : usize,
-    head : Option<Box<Node<T>>>,
+    pub len  : usize,
+    pub head : Option<Box<Node<T>>>,
 }
 
 impl<T> List<T> {

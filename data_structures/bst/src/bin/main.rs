ac559ab1d2d456f3f0d668cff986b97dd280c636
Arun M <arun11299@gmail.com>
2018-05-07 13:01:12 +0530

    Some code refactoring in remove function + added removal of right subtree but still lot of code duplication in remove subroutine

--- data_structures/bst/src/bin/main.rs
+++ data_structures/bst/src/bin/main.rs
@@ -6,20 +6,24 @@ use bst::Node;
 fn main() {
     let mut tree : BST<i32> = BST::new();
     tree.add(50); // the head
-    /*
-    if let Some(ref mut root) = tree.head {
-        BST::move_to_leftmost(root.as_raw(), Box::new(Node::new(5)));
-        BST::move_to_leftmost(root.as_raw(), Box::new(Node::new(6)));
-        BST::move_to_leftmost(root.as_raw(), Box::new(Node::new(7)));
-    }
-    */
     tree.add(20);
     tree.add(10);
     tree.add(30);
+    tree.add(60);
+    tree.add(70);
+    tree.add(55);
 
     tree.print(|elem| println!("{}", elem));
 
     tree.remove(20);
 
+    println!("------------------");
+
     tree.print(|elem| println!("{}", elem));
+
+    tree.remove(60);
+
+    println!("------------------");
+
+    tree.print(|elem| println!("{}", elem)); 
 }
